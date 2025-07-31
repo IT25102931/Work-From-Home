a911483f44a0164f8fe9ff9121a275bb119b8f3d
IT251029331 <it25102931@my.sliit.lk>
2025-07-31 20:25:54 +0530

    Hello_world_X

---
+++ Hello_worldx.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	printf("Hello! Welcome to SLIIT");
+	printf("First line\nThis is the second line\n");
+	printf("X         X\n");
+	printf("X   X\n");
+	printf("X            X\n");
+	printf("X   X\n");
+	printf("X            X\n");
+	printf("X   X\n");
+	printf("X            X\n");
+	printf("X   X\n");
+	printf("X            X\n");
+	printf("X         X\n");
+
+	return 0;
+} 
